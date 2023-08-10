37985af047c997e2bb11d35dacd1499e6319ca38
Bertrand Brown <133926757+BlacKrab666@users.noreply.github.com>
2023-08-10 18:32:43 -0500

    Update Class_Never_Object.py

--- Class_Never_Object.py
+++ Class_Never_Object.py
@@ -1,3 +1,4 @@
+### BlacKrab666 ###
 class Employee:
     name = 'Jack'
     empCount = 0
@@ -21,10 +22,15 @@ class Employee:
     def displayEmployee(self):
         print(f"Name : {self.name}", )
         
-Employee.ojb1 = Employee("Mavrik")
-damn = Employee.obj1()
+Employee.ojb1 = Employee("Mavrik") # even the only objectified code idea is thrown back into subjective reality
+damn = Employee.obj1() # Not an object
 # damn.displayEmployee()
 # damn.displayCount()
-print(damn)
-print(Employee)
-damn.ClassAssObjNotObj(damn) # must follow up and see why statement is not printing but so far no errors. Jack not here.
+print(damn)                     # <class '__main__.Employee'>
+print(Employee)                 # <class '__main__.Employee'>
+damn.ClassAssObjNotObj(damn)        # must follow up and see why statement is not printing but so far no errors. Jack not here.
+Employee.ClassAssObjNotObj(damn)    # must follow up and see why statement is not printing but so far no errors. Jack not here.
+print(Employee.obj1)            # <bound method Employee.obj1 of <class '__main__.Employee'>>
+print(Employee.obj1())          # <class '__main__.Employee'>
+
+# results show both ideas are the same, the projections of my mind minifest in reality with no objective reality
